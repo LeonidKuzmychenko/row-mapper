c8473a39fc3c7716d213c283a4eabe991c5e0db3
leonid.kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2021-02-15 17:08:20 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import row.RowMapper;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("1", null, "3", null, "5", "6");
+        Cat cat = new Cat("01", "02", null, "04", "05", "06");
+        System.out.println(dog);
+        dog = new RowMapper<Dog>().join(dog, cat);
+        System.out.println(dog);
+    }
+
+}
